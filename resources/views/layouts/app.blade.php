0ecccfea46baf201484d53ec977573d07736b3ec
safiull <safiullalam9931@gmail.com>
2022-08-04 22:30:47 +0600

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name', 'Library Management System') }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"> <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }} "> <!-- Custom stlylesheet -->
+</head>
+
+<body>
+    <div id="header">
+        <!-- HEADER -->
+        <div class="container">
+            <div class="row">
+                <div class="offset-md-4 col-md-4 pt-5 pb-4">
+                    <div class="logo">
+                        <a  href="#"><img height="100" src="http://182.160.97.196:8088/studentportal/assets/nub-big-logo-e909d7e3005bdaf2fed2701ad5fd998a.png"></a>
+                    </div>
+                </div>
+                <div class="offset-md-2 col-md-2">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Hi {{ auth()->user()->name }}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="{{ route('change_password') }}">Change Password</a>
+                            <a class="dropdown-item" href="#" onclick="document.getElementById('logoutForm').submit()">Log Out</a>
+                        </div>
+                        <form method="post" id="logoutForm" action="{{ route('logout') }}">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+
+                <div class="row m-auto">
+                    <div class="col">
+                        <h2 class="text-danger text-center mb-3 d-block">Library management system</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /HEADER -->
+    <div id="menubar">
+        <!-- Menu Bar -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="menu">
+                        <li class="{{ request()->is('dashboard') ? 'active':'' }}"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                        <li class="{{ request()->is('authors') ? 'active':'' }}"><a href="{{ route('authors') }}">Authors</a></li>
+                        <li class="{{ request()->is('publishers') ? 'active':'' }}"><a href="{{ route('publishers') }}">Publishers</a></li>
+                        <li class="{{ request()->is('categories') ? 'active':'' }}"><a href="{{ route('categories') }}">Categories</a></li>
+                        <li class="{{ request()->is('books') ? 'active':'' }}"><a href="{{ route('books') }}">Books</a></li>
+                        <li><a href="{{ route('students') }}">Reg Students</a></li>
+                        <li><a href="{{ route('book_issued') }}">Book Issue</a></li>
+                        <li><a href="{{ route('reports') }}">Reports</a></li>
+                        <li><a href="{{ route('settings') }}">Settings</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /Menu Bar -->
+
+    @yield('content')
+
+    <!-- FOOTER -->
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <span>© Copyright 2022 <a href="#">Team 06 😎</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /FOOTER -->
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.guest')
+@section('content')
+
+    <div id="wrapper-admin">
+        <div class="container">
+            <div class="row">
+                <div class="offset-md-4 col-md-4">
+                    <div class="logo border-danger mb-3">
+                        <img src="http://182.160.97.196:8088/studentportal/assets/nub-big-logo-e909d7e3005bdaf2fed2701ad5fd998a.png" alt="">
+                    </div>
+                    <form class="yourform" action="{{ route('login') }}" method="post">
+                        @csrf
+                        <h3 class="heading">Login</h3>
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="username" class="form-control" value="{{ old('username') }}"
+                                required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" value="" required>
+                        </div>
+                        <input type="submit" name="login" class="btn btn-danger" value="login" />
+                    </form>
+                    @error('username')
+                        <div class='alert alert-danger'>{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
